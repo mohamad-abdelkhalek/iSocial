6da8f38f6d0e94f4b4bac12160fd1e9a5da545e0
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-11-04 23:39:10 +0200

    Added animation for hero section

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,25 @@ gsap.from(".navbar-logo img", {
     stagger: 0.2
   });
 
+  gsap.from(".hero-img", {
+    opacity: 1,
+    scale: 0,
+    duration: 1.4,
+    ease: "power2.out",
+  });
+
+  gsap.from(".hero-content h2", {
+    opacity: 0,
+    y: 30,
+    duration: 1.2,
+    delay: 0.5,
+    ease: "power2.out",
+  });
+
+  gsap.from(".hero-content p", {
+    opacity: 0,
+    y: 30,
+    duration: 1.2,
+    delay: 0.7,
+    ease: "power2.out",
+  });
